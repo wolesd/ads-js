13c83d30fe1e1dfbbbbf43ad243ad472b0fdaff4
wolesd <71396368+wolesd@users.noreply.github.com>
2021-02-11 02:17:05 +0700

    Update wp-ucap.js

--- wp-ucap.js
+++ wp-ucap.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">var _Hasync= _Hasync|| [];
+var _Hasync= _Hasync|| [];
 _Hasync.push(['Histats.start', '1,4300589,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
@@ -6,5 +6,4 @@ _Hasync.push(['Histats.track_hits', '']);
 var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img src="//sstatic1.histats.com/0.gif?4300589&101" alt="web counter" border="0"></a></noscript>
+})();
